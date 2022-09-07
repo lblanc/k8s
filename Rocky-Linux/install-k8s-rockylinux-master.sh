990e8351c8f8b433c026423345345f35066ecc7e
Luc BLANC <Email@luc-blanc.com>
2022-09-08 00:01:07 +0200

    -

--- Rocky-Linux/install-k8s-rockylinux-master.sh
+++ Rocky-Linux/install-k8s-rockylinux-master.sh
@@ -19,13 +19,13 @@ sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
 
 # Configure Firewall
-sudo systemctl enable --now firewalld.service
-sudo firewall-cmd --permanent --add-port=6443/tcp
-sudo firewall-cmd --permanent --add-port=2379-2380/tcp
-sudo firewall-cmd --permanent --add-port=10250/tcp
-sudo firewall-cmd --permanent --add-port=10251/tcp
-sudo firewall-cmd --permanent --add-port=10252/tcp
-sudo firewall-cmd --reload
+sudo systemctl disable --now firewalld.service
+#sudo firewall-cmd --permanent --add-port=6443/tcp
+#sudo firewall-cmd --permanent --add-port=2379-2380/tcp
+#sudo firewall-cmd --permanent --add-port=10250/tcp
+#sudo firewall-cmd --permanent --add-port=10251/tcp
+#sudo firewall-cmd --permanent --add-port=10252/tcp
+#sudo firewall-cmd --reload
 sudo modprobe br_netfilter
 sudo sh -c "echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables"
 sudo sh -c "echo '1' > /proc/sys/net/ipv4/ip_forward"
