c4f828397944fc3b8535853ea082a79a19bdba1a
Luc BLANC <Email@luc-blanc.com>
2022-09-08 13:55:51 +0200

    -

--- mayastor/prerequisites.sh
+++ mayastor/prerequisites.sh
@@ -14,8 +14,10 @@ NC='\033[0m' # No Color
 #Enable Huge Page Support
 echo -e "${ORANGE}Enable Huge Page Support....${NC}"
 echo 1024 | sudo tee /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+echo vm.nr_hugepages = 1024 | sudo tee -a /etc/sysctl.con
 for node in ${nodes}; do
  
-  ssh root@${node} echo 1024 | sudo tee /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages      
+  ssh root@${node} echo 1024 | sudo tee /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+  ssh root@${node} echo vm.nr_hugepages = 1024 | sudo tee -a /etc/sysctl.con      
  
 done
