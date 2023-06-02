4e0c27a11568bf9eedbaca10cbda58f332aaeb81
Luc BLANC <Email@luc-blanc.com>
2023-06-02 10:56:55 -0300

    Update deploy-mayastor.sh

--- mayastor/deploy-mayastor.sh
+++ mayastor/deploy-mayastor.sh
@@ -17,11 +17,11 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 # Install helm
-#echo
-#echo "${YELLOW}install helm....${NC}"
-#tar -xvz  -f <(wget -q -O - https://get.helm.sh/helm-v3.12.0-linux-amd64.tar.gz) linux-amd64/helm
-#sudo mv linux-amd64/helm /usr/local/bin/helm
-#sudo rm -fR linux-amd64
+echo
+echo "${YELLOW}install helm....${NC}"
+tar -xvz  -f <(wget -q -O - https://get.helm.sh/helm-v3.12.0-linux-amd64.tar.gz) linux-amd64/helm
+sudo mv linux-amd64/helm /usr/local/bin/helm
+sudo rm -fR linux-amd64
 
 
 # Add the OpenEBS Mayastor Helm repository.
