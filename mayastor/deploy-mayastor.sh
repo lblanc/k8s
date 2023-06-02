be0c08b3c4c687521ad62bb9e47e92edf3ab0a1e
Luc BLANC <Email@luc-blanc.com>
2023-06-02 10:50:46 -0300

    Update deploy-mayastor.sh

--- mayastor/deploy-mayastor.sh
+++ mayastor/deploy-mayastor.sh
@@ -19,7 +19,7 @@ NC='\033[0m' # No Color
 # Install helm
 echo
 echo "${YELLOW}install helm....${NC}"
-ssh ${user}@${node} "tar -xvz  -f <(wget -q -O - https://get.helm.sh/helm-v3.12.0-linux-amd64.tar.gz) linux-amd64/helm
+ssh ${user}@${node} "tar -xvz  -f <(wget -q -O - https://get.helm.sh/helm-v3.12.0-linux-amd64.tar.gz) linux-amd64/helm"
 sudo mv linux-amd64/helm /usr/local/bin/helm
 sudo rm -fR linux-amd64
 
