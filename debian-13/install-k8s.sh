4555f4bb4c16dd2609f3400081f5d8da5c10723e
Luc BLANC <Email@luc-blanc.com>
2025-11-07 09:11:26 +0100

    Update install-k8s.sh

--- debian-13/install-k8s.sh
+++ debian-13/install-k8s.sh
@@ -161,4 +161,4 @@ sleep 10
 echo "${YELLOW}Install Flannel Pod Network Add-on ${NC}"
 ssh ${user}@${masternode} "kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml"
 
-./k9s
+k9s
