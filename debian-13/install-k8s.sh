ea983950e620f771bb8c2ab5246cab4024a0ad49
Luc BLANC <Email@luc-blanc.com>
2026-03-11 14:48:44 +0100

    Update Kubernetes version to 1.33 in install script

--- debian-13/install-k8s.sh
+++ debian-13/install-k8s.sh
@@ -112,10 +112,10 @@ ssh ${user}@${node} "sudo apt update"
 ssh ${user}@${node} "sudo apt install -y apt-transport-https ca-certificates curl gpg"
 
 ssh ${user}@${node} "sudo mkdir -p /etc/apt/keyrings"
-ssh ${user}@${node} "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg"
+ssh ${user}@${node} "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg"
 
-ssh ${user}@${node} "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] \
-https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list"
+
+ssh ${user}@${node} "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' |sudo  tee /etc/apt/sources.list.d/kubernetes.list"
 
 
 # Install Kubernetes components Kubectl, kubeadm & kubelet
